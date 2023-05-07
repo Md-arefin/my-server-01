a6143eb053647e021bdf77da6cb00a6d495f1a9c
Md-arefin <ansariarefin617@gmail.com>
2023-05-07 15:56:50 +0600

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// app.use(cors);
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const users = [
+    { id: 1, name: 'Shabana' , email: 'shabana@gmail.com'},
+    { id: 2, name: 'Sabnoor' , email: 'sabnoor@gmail.com'},
+    { id: 3, name: 'Sabila' , email: 'sabila@gmail.com'},
+]
+
 
 app.get("/", (req,res) =>{
     res.send("User Management Server Is Running")
 });
+
+app.get('/users', (req, res) =>{
+    res.send(users);
+})
+
+app.post('/users' , (req, res) =>{
+    console.log("post api hitting")
+    console.log(req.body);
+    const newUser = req.body;
+    newUser.id = users.length +1;
+    users.push(newUser);
+    res.send(newUser);
+})
 
 app.listen(port, () =>{
     console.log(`Server is running on port: ${port}`)
